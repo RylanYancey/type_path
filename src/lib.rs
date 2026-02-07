d644343cad065e68252f05110ce941d6150b9484
RylanYancey <rylanyancey@gmail.com>
2026-02-07 12:50:53 -0500

    Added short_type_path and into_short_type_path

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use crate::cell::GenericTypeCell;
+
 pub mod cell;
 
 #[cfg(feature = "uuid")]
@@ -22,6 +24,12 @@ pub trait TypePath: 'static {
     ///
     /// For `Option<Vec<usize>>`, this is `"std::option::Option<std::vec::Vec<usize>>"`.
     fn type_path() -> &'static str;
+
+    /// Returns the type name, without any paths on the outer or inner types.
+    fn short_type_path() -> &'static str {
+        static CELL: GenericTypeCell = GenericTypeCell::new();
+        CELL.get_or_insert::<Self, _, _>(|| into_short_type_path(Self::type_path()))
+    }
 }
 
 /// Dynamic Dispatch for [`TypePath`].
@@ -36,5 +44,50 @@ pub trait DynamicTypePath {
 impl<T: TypePath> DynamicTypePath for T {
     fn type_path(&self) -> &'static str {
         T::type_path()
+    }
+}
+
+/// Removes type paths from a string.
+/// For example, `std::option::Option<std::vec::Vec<std::u32>>` becomes `Option<Vec<u32>>`.
+fn into_short_type_path(s: &str) -> String {
+    let mut out = String::with_capacity(s.len() / 2);
+    let mut iter = s.split("::").peekable();
+    while let Some(next) = iter.next() {
+        if let Some(_) = iter.peek() {
+            // The two characters following `next` are ::.
+            if let Some(i) = next.rfind(|c: char| !c.is_alphanumeric()) {
+                out.push_str(&next[..=i]);
+            }
+        } else {
+            out.push_str(next);
+        }
+    }
+    out
+}
+
+#[cfg(test)]
+mod tests {
+    use super::into_short_type_path;
+
+    #[test]
+    fn test_into_short_type_path() {
+        assert_eq!(
+            &into_short_type_path("std::vec::Vec<std::option::Option<std::u32>>"),
+            "Vec<Option<u32>>"
+        );
+
+        assert_eq!(
+            &into_short_type_path(
+                "std::vec::Vec<std::option::Option<(std::u32, std::string::String)>>"
+            ),
+            "Vec<Option<(u32, String)>>"
+        );
+
+        assert_eq!(
+            &into_short_type_path(
+                "std::vec::Vec<std::collections::HashMap<std::string::String, std::result::Result<std::usize, std::u32>>>"
+            ),
+            "Vec<HashMap<String, Result<usize, u32>>>"
+        );
     }
 }
